f26ded4ecc05655b6358302e6e154b3f12d774b1
Splatage.com <81358782+splatage@users.noreply.github.com>
2025-09-16 20:37:12 +1200

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    java
+    `java-library`
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+    withJavadocJar()
+    withSourcesJar()
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
+    // No NMS imports; everything else via reflection
+}
+
+tasks.processResources {
+    filesMatching("paper-plugin.yml") {
+        expand("version" to (project.version as String))
+    }
+}
